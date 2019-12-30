0f9bdc255dd192e61cae31c21b4419b2698b6aff
zhangcan0712 <zhangcan0712@focuschina.com>
2019-12-30 17:40:24 +0800

    增加top_k和top_p的tf方法

--- transformer_contrib/keras_gpt_2/gen.py
+++ transformer_contrib/keras_gpt_2/gen.py
@@ -1,5 +1,9 @@
 import numpy as np
+import tensorflow as tf
 
+__all__ = [
+    'generate', 'top_k_logits', 'top_p_logits',
+]
 
 def generate(model,
              bpe,
@@ -38,3 +42,39 @@ def generate(model,
             input_data[index][text_lens[index] + shift] = next_token
     outputs = [bpe.decode(input_data[index][:text_lens[index] + length]) for index in range(batch_size)]
     return outputs
+
+
+def top_k_logits(logits, k):
+    if k == 0:
+    # no truncation
+        return logits
+
+    def _top_k():
+        values, _ = tf.nn.top_k(logits, k=k)
+        min_values = values[:, -1, tf.newaxis]
+        return tf.where(
+            logits < min_values,
+            tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+            logits,
+        )
+    return tf.cond(tf.equal(k, 0),
+                    lambda: logits,
+                    lambda: _top_k())
+
+
+def top_p_logits(logits, p):
+    """Nucleus sampling"""
+    batch, _ = logits.shape.as_list()
+    sorted_logits = tf.contrib.framework.sort(logits, direction='DESCENDING', axis=-1)
+    cumulative_probs = tf.cumsum(tf.nn.softmax(sorted_logits, axis=-1), axis=-1)
+    indices = tf.stack([
+        tf.range(0, batch),
+        # number of indices to include
+        tf.maximum(tf.reduce_sum(tf.cast(cumulative_probs <= p, tf.int32), axis=-1) - 1, 0),
+    ], axis=-1)
+    min_values = tf.gather_nd(sorted_logits, indices)
+    return tf.where(
+                logits < min_values,
+                tf.ones_like(logits) * -1e10,
+                logits,
+            )
